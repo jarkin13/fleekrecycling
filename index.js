8efa175325ef41cf9e1a04388f318e57f8f7a053
jarkin13 <jamielaurenarkin@gmail.com>
2016-05-26 22:02:15 -0400

    update server file

--- index.js
+++ index.js
@@ -8,9 +8,4 @@ app.get('/', function(req, res) {
 	res.send('Hello World!')
 });
 
-var server = app.listen(80, function() {
-	var host = server.address().address;
-  var port = server.address().port;
- 
-  console.log('Dashboard listening at http://%s:%s', host, port);
-});
+app.listen(process.env.PORT || 80);
