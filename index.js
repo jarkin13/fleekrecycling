831bf8d47a065a30183c59514e1c5378812dad1b
jarkin13 <jamielaurenarkin@gmail.com>
2016-05-26 22:09:48 -0400

    testing again

--- index.js
+++ index.js
@@ -8,8 +8,4 @@ app.get('/', function(req, res) {
 	res.send('Hello World!')
 });
 
-app.on('listening', function() {
-	console.log('ok, server is running');
-});
-
-app.listen(process.env.PORT || 80);
+console.log(server.address());
